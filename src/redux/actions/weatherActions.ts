9974b76dffca7a0b2f66c7225db6f15b7b07fed6
Niko Lepka <slench102+git@gmail.com>
2022-09-30 16:07:44 +0200

    hide api key

--- src/redux/actions/weatherActions.ts
+++ src/redux/actions/weatherActions.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { Dispatch } from "react";
 import { OWMResponseDto } from "../../types/open-weather-map-response-dto";
-
+import { APIKey } from "../../APIKEY";
 export const getWeather = () => {
   return async (dispatch: Dispatch<any>) => {
     const [lat, lon] = [57.019602121561995, 9.883959999263695]; // seluxit coordinates
-    const APIKey = "2f22021dd587f35f5a9f541f06756aee";
     try {
       const response = await axios.get<OWMResponseDto>(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`
